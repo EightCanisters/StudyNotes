0861b3d56962f7ce4e25932868bab43ed5ca7193
linxiu <ahuang6328@163.com>
2021-06-24 23:34:36 +0800

    Update: 将完整大demo转移到demos

--- Technology Stack/Localization/i18next/示例/react-localization/src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/interface-name-prefix
-  interface Window {
-    appContext: AppContext;
-  }
-}
