4e3d95621b77411a874b33a309e0015649095bc1
linxiu <ahuang6328@163.com>
2021-07-11 22:58:15 +0800

    webpack: devserver

---
+++ Webpack/2.webpack开发环境配置/07.devServer/webpack.config.js
@@ -0,0 +1,51 @@
+
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/indes.js',
+  output: {
+    filename: 'built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        exclude: /\.(css|js|html|png|gif|jpg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  mode: 'development',
+  /**
+   * devServer
+   *    是什么：开发服务器，用来自动化（自动编译，自动打开浏览器，自动刷新浏览器）;
+   *    特点：只会在内存中编译打包，不会有任何输出;
+   *    如何启动：npx webpack serve(webpack-cli: 4.x), npx webpack-dev-server(webpack-cli 3.x);
+   */
+  devServer: {
+    contentBase: resolve(__dirname, 'build'), // 项目构建后的路径
+    compress: true, // 是否使用gzip压缩
+    port: 3000, // 设置端口号
+    open: true // 是否自动打开浏览器
+  }
+}
