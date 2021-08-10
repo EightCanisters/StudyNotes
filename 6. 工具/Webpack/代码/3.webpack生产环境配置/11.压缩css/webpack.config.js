4ab167d600af2b2d9082366befda74f3a2185b13
linxiu <ahuang6328@163.com>
2021-08-10 22:26:14 +0800

    【webpack】压缩css

---
+++ 6. 工具/Webpack/代码/3.webpack生产环境配置/11.压缩css/webpack.config.js
@@ -0,0 +1,47 @@
+const { resolve } = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+
+// 配置node的环境变量
+// process.env.NODE_ENV = 'development';
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [
+                  'postcss-preset-env'
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCSSExtractPlugin({
+      filename: 'css/built.css'
+    }),
+    new OptimizeCSSAssetsWebpackPlugin()
+  ],
+  mode: 'development'
+}
